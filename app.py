85c8885957fe51ba548f3897a642da24b1bf8098
theahihi <168960744+theahihi@users.noreply.github.com>
2024-07-10 19:58:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
