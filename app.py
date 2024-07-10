f5004edd79d2beb38c1a7178ac8919f4c2ae4ef1
theahihi <168960744+theahihi@users.noreply.github.com>
2024-07-10 19:58:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
